960c2f85b19448d3851ba813d834686e05c49247
eunsukimme <eunsu.dev@gmail.com>
2021-02-27 23:59:35 +0900

    Remove deprecated prettier plugin

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
   extends: [
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended',
   ],
   rules: {
